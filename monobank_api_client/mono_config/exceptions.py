60d40807fca941becc4e6533ec23cbd0c0c02d6b
Ihor Sotnyk <ihor.sotnyk@onix-systems.com>
2024-01-19 15:47:00 +0200

    fix(repository): Debugging

--- monobank_api_client/mono_config/exceptions.py
+++ monobank_api_client/mono_config/exceptions.py
@@ -2,14 +2,14 @@ from typing import List, Dict
 
 
 class MonoException:
-    def __init__(self) -> None:
-        pass
-
     @staticmethod
     def currency_error(list_ccy: List) -> Dict:
-        error_response = {
-            "code": 400,
-            "detail": "Incorrect currency pair",
-            "list of acceptable currency pairs": list_ccy,
-        }
-        return error_response
+        try:
+            error_response = {
+                "code": 400,
+                "detail": "Incorrect currency pair",
+                "list of acceptable currency pairs": list_ccy,
+            }
+            return error_response
+        except Exception as exc:
+            error_response = {"detail": str(exc)}
